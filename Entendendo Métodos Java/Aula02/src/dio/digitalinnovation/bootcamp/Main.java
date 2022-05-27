9386a588fbbbe629334887d580c1059d0017da19
João Vitor Felix de Andrade <joao.felixandrade1201@gmail.com>
2022-05-26 21:41:10 -0300

    Aulas 02 e 03 do curso Entendendo Métodos Java

---
+++ Entendendo Métodos Java/Aula02/src/dio/digitalinnovation/bootcamp/Main.java
@@ -0,0 +1,13 @@
+package dio.digitalinnovation.bootcamp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Quadrilátero
+        System.out.println("Exercício Quadrilátero\n");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7, 6, 9);
+        Quadrilatero.area(3f, 5f);
+    }
+}
---
+++ Entendendo Métodos Java/Aula03/src/dio/digitalinnovation/bootcamp/Main.java
@@ -0,0 +1,12 @@
+package dio.digitalinnovation.bootcamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Exercício Quadrilátero\n");
+        System.out.println("Área do quadrado: "+Quadrilatero.area(2));
+        System.out.println("Área do retângulo: "+Quadrilatero.area(3,7));
+        System.out.println("Área do trapézio: "+Quadrilatero.area(9, 5, 3));
+    }
+}
