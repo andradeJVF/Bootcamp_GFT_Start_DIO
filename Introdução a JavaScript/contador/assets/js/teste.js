226590469790dde815eeaf9863e294870887e5c7
João Vitor Felix de Andrade <joao.felixandrade1201@gmail.com>
2022-06-28 08:53:15 -0300

    Iniciando curso de Introdução a JavaScript

---
+++ Introdução a JavaScript/contador/assets/js/teste.js
@@ -0,0 +1,15 @@
+function returnEvenValues(array){
+    let evenNums = [];
+    for(let i = 0; i < array.length; i++){
+        if(array[i] % 2 === 0){
+            evenNums.push(array[i]);
+        } else{
+            console.log(`${array[i]} esse número não é par!`)
+        }
+    }
+    console.log("Os números a seguir são pares:",evenNums)
+}
+
+let array = [1, 2, 3, 4, 5, 6];
+
+returnEvenValues(array);
